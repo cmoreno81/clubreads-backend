eb9820bf9d3e7da6bc51329524e779baa7e3b591
Cristina Moreno <c.moreno.benavente@gmail.com>
2026-07-16 16:51:41 +0200

    Configuración para subida de fotos

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,11 @@ import { apiRouter } from './routes/api.router.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(
+  express.json({
+    limit: '5mb',
+  }),
+);
 app.use('/api', apiRouter);
 
 const port = Number(process.env.PORT) || 3000;
